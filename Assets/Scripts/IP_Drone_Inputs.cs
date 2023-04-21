5f8f52f887ed5199a36e4cfd9d53f96796aca631
LAPTOP-SVEN\Sven <svenboeckx2@gmail.com>
2023-04-21 20:11:44 +0200

    Section 2 completed

---
+++ Assets/Scripts/IP_Drone_Inputs.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace IndiePixel
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class IP_Drone_Inputs : MonoBehaviour
+    {
+        #region Variables
+        private Vector2 _cyclic = Vector2.zero;
+        private float _pedals = 0.0f;
+        private float _throttle = 0.0f;
+
+        public Vector2 Cyclic { get => _cyclic; }
+        public float Pedals { get => _pedals; }
+        public float Throttle { get => _throttle; }
+        #endregion
+
+        #region Input Methods
+        private void OnCyclic(InputValue value)
+        {
+            _cyclic = value.Get<Vector2>();
+        }
+
+        private void OnPedals(InputValue value)
+        {
+            _pedals = value.Get<float>();
+        }
+
+        private void OnThrottle(InputValue value)
+        {
+            _throttle = value.Get<float>();
+        }
+        #endregion
+    }
+}
